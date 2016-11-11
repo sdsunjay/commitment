365a1de129a839690866c5524af4650fa102785a
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-11-11 15:37:21 -0800

    Everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
-end
+
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  has_many :challenges
+  has_many :answers
+  has_many :courses, through: :enrollments, dependent: :destroy
+
+  validates_format_of :email, without: /\Achange@me/, on: :update
+  validates :password, length: { minimum: 8, maximum: 64 }, unless: "password.nil?"
+  validates :password, presence: true, if: "id.nil?"
+
+  validates :name, presence: { message: 'You must enter your name'}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.edu))\z/i, on: :create }
+
+  validates_uniqueness_of :email
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+    devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks"}
+
+    authenticate :user do
+        resources :enrollments, only: [:show, :index, :new, :create, :edit, :update] do
+            resources :courses, only: [:show, :index, :new, :create] do
+                resources :challenges, only: [:new, :create, :index, :edit, :update, :destroy, :show] do
+                   resources :answers, only: [:new, :create, :index, :edit, :update]
+                end
+            end
+        end
+        resources :users, only: [:show, :index, :edit, :update]
+    end
   root 'home#index'
 end
