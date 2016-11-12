1d5cb80af1881b763f9351965ed513fdfe1baeeb
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-11-11 20:52:14 -0800

    Added identity model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111223819) do
+ActiveRecord::Schema.define(version: 20161112044828) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id",      null: false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20161111223819) do
     t.integer  "score",      default: 0, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "schools", force: :cascade do |t|
