d07ab6efeaaf76491e63afaa4f1dfdcdb8f93b70
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-11-11 17:28:27 -0800

    Seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+school1 = School.new({:name => "California Polytechnic State University", :address => "San Luis Obispo, CA 93407"}).save
+school2 = School.new({:name => "Santa Clara University", :address => "500 El Camino Real, Santa Clara, CA 95053"}).save
+school3 = School.new({:name => "University of California, Santa Barbara", :address => "Santa Barbara, CA 93106"}).save
+user1 = User.create :name => "Sunjay Dhama", :school_id => 0,  :last_sign_in_at => DateTime.strptime("01/01/2016 17:00", "%m/%d/%Y %H:%M"), :email => "sunjay@example.com", :password => "password"
+user2 = User.create :name => "Petar Georgiev", :school_id => 0, :last_sign_in_at => DateTime.strptime("01/01/2016 17:00", "%m/%d/%Y %H:%M"), :email => "petar@example.com", :password => "password"
+user3 = User.create :name => "Elliot Fiske", :school_id => 0, :last_sign_in_at => DateTime.strptime("01/01/2016 17:00", "%m/%d/%Y %H:%M"), :email => "elliot@example.com", :password => "password"
