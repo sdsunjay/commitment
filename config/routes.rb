ab03283369d91fc5e7d5f0cf4d18dc42b8c8c43a
Peter Georgiev <georgiev2592@gmail.com>
2016-11-11 20:01:40 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-    devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks'}
-    authenticate :user do
-        resources :enrollments, only: [:show, :index, :new, :create, :edit, :update] do
-            resources :courses, only: [:show, :index, :new, :create] do
-                resources :challenges, only: [:new, :create, :index, :edit, :update, :destroy, :show] do
-                   resources :answers, only: [:new, :create, :index, :edit, :update]
-                end
-            end
-        end
-        resources :users, only: [:show, :index, :edit, :update]
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks'}
+  authenticate :user do
+    resources :answers, only: [:index, :edit, :update]
+    resources :challenges, only: [:index, :edit, :update]
+    resources :courses, only: [:show, :index, :new, :create] do
+      resources :challenges, only: [:new, :create] do
+        resources :answers, only: [:new, :create]
+      end
     end
-    root 'home#index'
+    resources :users, only: [:show, :index, :edit, :update]
+  end
+  root 'home#index'
 end
