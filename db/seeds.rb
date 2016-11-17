c918a9757fd4b93580ff9602872905e7c828d846
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-11-16 20:26:16 -0800

    added more schools and classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
-#   Mayor.create(name: "Emanuel", city: cities.first)
-
 courses = Course.create ([
 {name: "Introduction to General Pschology", course_number: "PSY-201", description: "Introduction to the psychological study of human beings. Applications and research in area such as psychobiology, perception, learning, motivation, consciousness, memory and cognition, personality, emotion, development, psychological assessment, social behavior, psychopathology and psychotherapy. A student may enroll for credit in either PSY 201 or PSY 202, but not both. 4 lectures. Fulfills GE D4."},
 {name: "Interpersonal Communication.", course_number: "PSY-212", description: "Introduction to the interaction process in two-person (dyadic) communication settings. Emphasis on the functions of varying messages in the initiation, development, maintenance and termination of personal and professional relationships. 4 lectures. Crosslisted as COMS/PSY 212."},
 {name: "Social Psychology", course_number: "PSY-252", description: "How attitudes, beliefs, and behavior are affected by the social situation. Gender roles, prejudice, aggression, altruism, attitudes and persuasion, liking and loving, and group behavior. Use of social psychology to understand diversity issues, reduce racism and sexism and international conflict, improve relationships, and communicate persuasively. 4 lectures."},
 {name: "Family Psychology", course_number: "PSY-254", description: "Introduction to research and theory on family relationships and behavior across the lifespan. Contextual influences, diversity of family forms, and topics such as love, mate selection, marital quality, parenting, gender, household work, divorce, and remarriage. 4 lectures. Crosslisted as CD/PSY 254."},
+{name: "Family Psychology 2", course_number: "PSY-255", description: "Introduction to research and theory and others on family relationships and behavior across the lifespan. Contextual influences, diversity of family forms, and topics such as love, mate selection, marital quality, parenting, gender, household work, divorce, and remarriage. 4 lectures. Crosslisted as CD/PSY 254."},
 {name: "Developmental Psychology", course_number: "PSY-256", description: "Introduction to the scientific study of development with emphasis on the lifespan, from infancy to old age. Basic research and concepts in understanding social, emotional, cognitive, contextual, and diversity influences on development. 4 lectures. Crosslisted as CD/PSY 256."}])
 schools = School.create ([
 	{name: "California Polytechnic State University", address: "San Luis Obispo, CA 93407"},
 	{name: "Santa Clara University", address: "500 El Camino Real, Santa Clara, CA 95053"},
+	{name: "San Jose State University", address: "1 Washington Sq, San Jose, CA 95192"},
+	{name: "University of California, Berkeley", address: "Berkeley, CA 94720"},
 	{name: "University of California, Santa Barbara", address: "Santa Barbara, CA 93106"}])
 
 users = User.create!([
